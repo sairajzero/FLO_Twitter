81fa29292cd7e4d1d2d0c626428addead90a1fda
sairajzero <sairaj0630@gmail.com>
2023-03-18 01:36:43 +0530

    Update tweeter.js
    - Added search algorithms to find users
    - Added fn to register user
    - Added: startup init fn

--- public/scripts/tweeter.js
+++ public/scripts/tweeter.js
@@ -33,26 +33,34 @@
     //fn to return onion address of target userID
     function getUserURL(userID) {
         return new Promise((resolve, reject) => {
-
+            compactIDB.readData("users", userID).then(result => {
+                if (result)
+                    resolve(result.address);
+                else
+                    reject(CustomError(CustomError.BAD_RESPONSE_CODE, "UserID not found in IDB", errorCode.USER_NOT_FOUND))
+            })
         })
     }
 
     const errorCode = floTwitter.errorCode = {
 
         INVALID_SERVER: '000',
-    
+
         //INVALID INPUTS: 0XX
         INVALID_REQUEST_FORMAT: '001',
-        MISSING_PARAMETER: '099',
         ACCESS_DENIED: '002',
         INVALID_FLO_ID: '011',
+        INVALID_CHARACTERS: '012',
         INVALID_PRIVATE_KEY: '013',
         INVALID_PUBLIC_KEY: '014',
         INVALID_SIGNATURE: '015',
         EXPIRED_SIGNATURE: '016',
         DUPLICATE_SIGNATURE: '017',
+        MISSING_PARAMETER: '099',
+
         //INCORRECT DATA: 1XX
-    
+        USER_NOT_FOUND: '100',
+
         //OTHERS
         NODES_OFFLINE: '404',
         INTERNAL_ERROR: '500'
@@ -237,6 +245,147 @@
             return responseParse(fetch_self('/following'));
         else
             return responseParse(fetch_target(userID, '/following'));
+    }
+
+    const validateName = floTwitter.validateName = function (name) {
+        return (typeof name === 'string') &&
+            /^[\w ]+$/.test(name) &&
+            !floCrypto.validateAddr(name);
+    }
+
+    const USER_REG_KEY = 'user_register';
+
+    floTwitter.registerUser = function (userID, name, address, privateKey) {
+        return new Promise((resolve, reject) => {
+            let user_floID = floCrypto.toFloID(userID);
+            if (floCrypto.verifyPrivKey(privateKey, user_floID))
+                return reject(CustomError(CustomError.BAD_REQUEST_CODE, "Invalid Private key", errorCode.INVALID_PRIVATE_KEY))
+            if (!validateName(name))
+                return reject(CustomError(CustomError.BAD_REQUEST_CODE, "Invalid name", errorCode.INVALID_CHARACTERS))
+            let words = name.toLowerCase().split(' ');
+            let receivers = words.map(w => floCrypto.hashID(w));
+            let pubkey = floCrypto.getPubKeyHex(privateKey);
+            let data = JSON.stringify({ [floGlobals.application]: { [USER_REG_KEY]: { id: userID, name, pubkey, address } } })
+            floBlockchainAPI.writeDataMultiple([privateKey], data, receivers)
+                .then(result => resolve(result))
+                .catch(error => reject(error))
+        })
+    }
+
+    floTwitter.searchUsers = function (name) {
+        return new Promise((resolve, reject) => {
+            if (!validateName(name))
+                return reject(CustomError(CustomError.BAD_REQUEST_CODE, "Invalid name", errorCode.INVALID_CHARACTERS))
+            let words = name.toLowerCase().split(' ');
+            let search_ids = words.map(w => floCrypto.hashID(w)),
+                application = floGlobals.application;
+            floBlockchainAPI.readData(search_ids.join(','), { pattern: application, tx: true }).then(result => {
+                var search_result = [], highest_match = 0;
+                for (let tx of result.data) {
+                    var content = JSON.parse(tx.data)[application];
+                    if (USER_REG_KEY in content) {
+                        let { id, name, pubkey, address } = content[USER_REG_KEY];
+                        let user_floID = floCrypto.toFloID(id);
+                        if (tx.senders.has(user_floID)) {
+                            let match = search_ids.reduce((a, e) => tx.receivers.has(e) ? ++a : a, 0);
+                            if (match == 0 || match < highest_match) //Do nothing
+                                continue;
+                            if (match == highest_match) //match is same, append user profile to list
+                                search_result.push({ id, name, pubkey, address });
+                            else if (match > highest_match) { //match is higher than current list, replace the list
+                                search_result = [{ id, name, pubkey, address }];
+                                highest_match = match;
+                            }
+                        }
+                    }
+                }
+                resolve(search_result);
+            }).catch(error => reject(error))
+        })
+    }
+
+    const getUser = floTwitter.getUser = function (userID) {
+        return new Promise((resolve, reject) => {
+            if (!floCrypto.validateAddr(userID))
+                return reject(CustomError.BAD_REQUEST_CODE, "Invalid userID", errorCode.INVALID_FLO_ID);
+            let user_floID = floCrypto.toFloID(userID),
+                application = floGlobals.application;
+            compactIDB.readData("lastTx", user_floID).then(lastTx => {
+                floBlockchainAPI.readData(user_floID, {
+                    ignoreOld: lastTx,
+                    sentOnly: true,
+                    pattern: application
+                }).then(result => {
+                    let user_details;
+                    for (let i in result.data) {
+                        var content = JSON.parse(result.data[i])[application];
+                        if (USER_REG_KEY in content) {
+                            if (floCrypto.isSameAddr(content[USER_REG_KEY].id, user_floID)) {
+                                user_details = {
+                                    id: content[USER_REG_KEY].id,
+                                    name: content[USER_REG_KEY].name,
+                                    address: content[USER_REG_KEY].address,
+                                }
+                            }
+                        }
+                    }
+                    if (user_details) //new user details found in blockchain, write and resolve the details
+                        Promise.all([
+                            compactIDB.writeData("users", user_details, user_floID),
+                            compactIDB.writeData("lastTx", result.totalTxs, user_floID)
+                        ]).then(result => resolve(user_details)).catch(error => reject(error))
+                    else //no new user details found in blockchain, check in IDB then resolve/reject if found
+                        compactIDB.readData("users", user_floID).then(user_details => {
+                            if (user_details)
+                                resolve(user_details);
+                            else
+                                reject(CustomError(CustomError.BAD_RESPONSE_CODE, "User not found", errorCode.USER_NOT_FOUND))
+                        })
+                }).catch(error => reject(error))
+            }).catch(error => reject(error))
+        })
+    }
+
+    function initUserDB() {
+        return new Promise((resolve, reject) => {
+            var obj = {
+                tweets: {},
+                messages: {},
+                addresses: {},
+                users: {},
+                lastTx: {},
+                appendix: {},
+                userSettings: {}
+            }
+            let user_db = `${floGlobals.application}_${floCrypto.toFloID(user.id)}`;
+            compactIDB.initDB(user_db, obj).then(result => {
+                console.log(result)
+                compactIDB.setDefaultDB(user_db);
+                resolve("floTwitter UserDB Initated Successfully")
+            }).catch(error => reject(error));
+        })
+    }
+
+    floTwitter.init = function () {
+        return new Promise((resolve, reject) => {
+            initUserDB().then(result => {
+                getUser(floDapps.user.id).then(details => {
+                    console.debug(window.location, details.address)
+                    responseParse(fetch_self('/user')).then(server => {
+                        if (floCrypto.isSameAddr(server.floID, floDapps.user.id)) {
+                            if (details.address === self_server_url)
+                                resolve(details);
+                            else resolve(false); //registered address is different from current
+                        } else
+                            reject(CustomError(CustomError.BAD_REQUEST_CODE, "Incorrect Server", errorCode.ACCESS_DENIED))
+                    }).catch(error => reject(error))
+                }).catch(error => {
+                    if (error instanceof CustomError && error.code === errorCode.USER_NOT_FOUND)
+                        return resolve(null);   //user not registered
+                    else reject(error);
+                })
+            }).catch(error => reject(error))
+        })
     }
 
 })('object' === typeof module ? module.exports : window.floTwitter = {});
