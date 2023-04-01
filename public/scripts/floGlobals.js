6a91812fe2daafe3350cf321fc64b3d3d0d4ff4b
sairajzero <sairaj0630@gmail.com>
2023-04-01 15:58:05 +0530

    Add floGlobals.js

---
+++ public/scripts/floGlobals.js
@@ -0,0 +1,6 @@
+/* Constants for FLO blockchain operations !!Make sure to add this at begining!! */
+const floGlobals = {
+    blockchain: "FLO",
+    adminID: "FKAEdnPfjXLHSYwrXQu377ugN4tXU7VGdf",
+    application: "Tweet_test",
+}
